c2dbbfb56c6cc8e76af7d8e2547e1236a8c4f3c1
Parth Gulati <gulati.parth98@gmail.com>
2021-05-16 16:02:46 +0530

    Added stylesheets, created Layout for project

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      {children}
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "DJ Events | Find the hottest parties",
+  description: "Find the latest DJ and other musical events",
+  keywords: "music, dj, edm, events",
+};
