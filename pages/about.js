ca4190d523290a898b842b6b78d48107751d0b4b
Parth Gulati <gulati.parth98@gmail.com>
2021-05-16 16:55:13 +0530

    Added pages for listing, viewing and adding events, enwrapped them with Layout

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/Layout";
 
 export default function AboutPage() {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>This is a basic about page</p>
-        </div>
-    )
+  return (
+    <Layout title="About DJ Events">
+      <h1>About</h1>
+      <p>This is a basic about page</p>
+    </Layout>
+  );
 }
---
+++ pages/events/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function events() {
+  return (
+    <Layout>
+      <h1>My Events</h1>
+    </Layout>
+  );
+}
