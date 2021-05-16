0631aeb3a89101ab5ad21aadae5ef7ca6652d8b5
Parth Gulati <gulati.parth98@gmail.com>
2021-05-16 22:04:13 +0530

    Fixed API response rendering

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,29 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout from "@/components/Layout";
 import styles from "@/styles/Home.module.css";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function HomePage({ events }) {
+  console.log(events);
+  console.log(events.events);
+  let eventArray = events.events;
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Upcoming Events</h3>}
+      {eventArray.map((x) => (
+        <h3 key={x.id}>{x.name}</h3>
+      ))}
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
-  const res = await fetch("/api/events");
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
