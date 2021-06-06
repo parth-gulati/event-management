866e64d3129c949edb332fb76962362955cb19ff
Parth Gulati <gulati.parth98@gmail.com>
2021-06-07 02:28:47 +0530

    Added pagination component, completed image upload modal

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -10,6 +10,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import moment from "moment";
 import Modal from "@/components/Modal";
+import ImageUpload from "@/components/ImageUpload";
 
 export default function EditEventPage({ evt }) {
   const router = useRouter();
@@ -61,6 +62,13 @@ export default function EditEventPage({ evt }) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
+  };
+
+  const imageUploaded = async (e) => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`);
+    const data = await res.json();
+    setImagePreview(data.image.formats.thumbnail.url);
+    setShowModal(false);
   };
 
   return (
@@ -159,7 +167,7 @@ export default function EditEventPage({ evt }) {
         </button>
       </div>
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        IMAGE UPLOAD
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
       </Modal>
     </Layout>
   );
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,12 @@
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import EventItem from "@/components/EventItem";
+import Link from "next/link";
+const PER_PAGE = 5;
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
+  const lastPage = Math.ceil(total / PER_PAGE);
+
   return (
     <Layout>
       <h1>Upcoming Events</h1>
@@ -12,16 +16,35 @@ export default function EventsPage({ events }) {
           {x.name}
         </EventItem>
       ))}
+
+      {page > 1 && (
+        <Link href={`/events?page=${page - 1}`}>
+          <a className="btn-secondary">Prev</a>
+        </Link>
+      )}
+      {page < lastPage && (
+        <Link href={`/events?page=${page + 1}`}>
+          <a className="btn-secondary">Next</a>
+        </Link>
+      )}
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
+export async function getServerSideProps({ query: { page = 1 } }) {
+  //Total
+  const totalRes = await fetch(`${API_URL}/events/count`);
+
+  const total = await totalRes.json();
+
+  //Calculate start page
+  const start = +page === 1 ? 0 : (page - 1) * PER_PAGE;
+  const res = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
   const events = await res.json();
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   };
 }
