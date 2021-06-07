60a92e4e96665b3ca44790c39b29063a9b31f402
Parth Gulati <gulati.parth98@gmail.com>
2021-06-07 16:19:53 +0530

    Created login and register page basic structure

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "@/styles/Header.module.css";
 import Link from "next/link";
 import Search from "./Search";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 export default function Header() {
   return (
@@ -23,7 +24,14 @@ export default function Header() {
           </li>
           <li>
             <Link href="/events/add">
-              <a>Add Events</a>
+              <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/account/login">
+              <a className="btn-secondary btn-icon">
+                <FaSignInAlt /> Login
+              </a>
             </Link>
           </li>
         </ul>
