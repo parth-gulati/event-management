51691a5e3a75b37833fd15b3bfdf31f49275e8e0
Parth Gulati <gulati.parth98@gmail.com>
2021-06-08 15:53:39 +0530

    Created AuthContext, added Context path to jsconfig.js

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -10,9 +10,30 @@ export const AuthProvider = ({ children }) => {
 
   //Register User
 
-  const register = async (user) => {};
+  const register = async (user) => {
+    console.log(user);
+  };
 
   //Login User
+  const login = async ({ email: identifier, password }) => {
+    console.log({ identifier, password });
+  };
 
   //Logout User
+  const logout = async () => {
+    console.log("Logout");
+  };
+
+  //Check if user is logged in
+  const checkUserLoggedIn = async (user) => {
+    console.log("Check");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
+
+export default AuthContext;
